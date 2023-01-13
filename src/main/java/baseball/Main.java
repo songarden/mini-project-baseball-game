7c24866471da702163dff868fcd4027831f22bb9
Son Jung Won <gkdlqkql6@naver.com>
2023-01-13 18:28:24 +0900

    230113 Dependency

--- src/main/java/baseball/Main.java
+++ src/main/java/baseball/Main.java
@@ -1,7 +1,28 @@
 package baseball;
 
+import com.github.javafaker.Faker;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("H");
+        Faker faker = new Faker();
+
+        Integer[] nums = Stream.generate(()->faker.number().randomDigitNotZero())
+                .distinct()
+                .limit(3)
+                .toArray(Integer[]::new);
+//        long r = faker.number().randomNumber();
+//        String title = faker.starTrek().character();
+//        System.out.println(Arrays.toString(nums));
+
+        User user = new User();
+        User user1 = new User();
+        user1.setAge(2);
+        System.out.println(user.equals(user1));
+        System.out.println(user.getAge());
+
+
     }
 }
---
+++ src/main/java/baseball/User.java
@@ -0,0 +1,11 @@
+package baseball;
+
+
+import lombok.*;
+
+@Data
+public class User {
+    private int age;
+    private String name;
+
+}
